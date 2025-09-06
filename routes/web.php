0f03090fbd63828637b83952e0c73adc1b293782
Moreeh Ahmed <146959594+Moreeh97@users.noreply.github.com>
2025-09-06 18:20:02 +0300

    feat: Add routes for all store pages

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TemplateController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StoreController;
+
 
 Route::get('/', [TemplateController::class, 'index']);
-Route::get('/products', [TemplateController::class, 'show']);
-Route::get('/product-details', [TemplateController::class, 'details']);
-Route::get('/about-us', [TemplateController::class, 'about']);
-Route::get('/contact', [TemplateController::class, 'contact']);
-
+Route::get('/products', [StoreController::class, 'products'])->name('products');
+Route::get('/product/{id}', [StoreController::class, 'productDetails'])->name('product.details');
+Route::get('/cart', [StoreController::class, 'cart'])->name('cart');
+Route::get('/contact', [StoreController::class, 'contact'])->name('contact');
+Route::get('/about-us', [StoreController::class, 'about'])->name('about-us');
